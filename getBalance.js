bc8bb868793b9c1bf057516ec269728a1de5e351
小渕周 <shukobuchi@gmail.com>
2022-07-18 16:01:56 +0900

    add wsProvider and package.json

--- getBalance.js
+++ getBalance.js
@@ -1,12 +1,13 @@
 // Import the API
-const { ApiPromise } = require('@polkadot/api');
+const { ApiPromise, WsProvider } = require('@polkadot/api');
 
 // Our address for Alice on the dev chain
 const ALICE = '5D7BtZu8HmTiCsMzkwH7EjFqphJEq4Lk6AHpighPVuABRhxG';
 
 async function main () {
+  const wsProvider = new WsProvider('wss://rpc.polkadot.io');
   // Create our API with a default connection to the local node
-  const api = await ApiPromise.create();
+  const api = await ApiPromise.create({ provider: wsProvider });
 
   // Make our basic chain state/storage queries, all in one go
   const [{ nonce: accountNonce }, { data: balance }, now ] = await Promise.all([
