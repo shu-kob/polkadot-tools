8957141c5ca48d6a7ab80a811f714890f9802fff
小渕周 <shukobuchi@gmail.com>
2023-02-22 23:00:11 +0900

    add Conditional branch of networks

--- getChainInformation.js
+++ getChainInformation.js
@@ -1,9 +1,25 @@
 // Import the API
 import { ApiPromise, WsProvider } from '@polkadot/api';
 
+let network = process.argv[2];
+
+const wsProvider = new WsProvider('wss://rpc.polkadot.io');
+
 async function main () {
   // Create connection to websocket
-  const wsProvider = new WsProvider('wss://rpc.polkadot.io');
+  if (network == 'kusama'){
+    const wsProvider = new WsProvider('wss://kusama-rpc.polkadot.io');
+  } else if (network == 'westend'){
+    const wsProvider = new WsProvider('wss://westend-rpc.polkadot.io');
+  } else if (network == 'rococo'){
+  const wsProvider = new WsProvider('wss://rococo-rpc.polkadot.io');
+  } else if (network == 'astar'){
+    const wsProvider = new WsProvider('wss://rpc.astar.network');
+  } else if (network == 'shibuya'){
+    const wsProvider = new WsProvider('wss://rpc.shibuya.astar.network');
+  } else {
+    const wsProvider = new WsProvider('wss://rpc.polkadot.io');
+  }
   // Create a new instance of the api
   const api = await ApiPromise.create({ provider: wsProvider });
   // get the chain information
