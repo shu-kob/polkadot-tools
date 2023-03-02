b07ed39ee0a56240bf51de5ffb184e52123eec91
小渕周 <shukobuchi@gmail.com>
2023-03-02 23:44:02 +0900

    fix getRuntime

--- getRuntimeVersion.js
+++ getRuntimeVersion.js
@@ -3,23 +3,24 @@ import { ApiPromise, WsProvider } from '@polkadot/api';
 
 let network = process.argv[2];
 
-const wsProvider = new WsProvider('wss://rpc.polkadot.io');
+let endpoint = '';
 
 async function main () {
 
-  if (network == 'polkadot'){
-    const wsProvider = new WsProvider('wss://rpc.polkadot.io');
-  } else if (network == 'kusama'){
-    const wsProvider = new WsProvider('wss://kusama-rpc.polkadot.io');
+  if (network == 'kusama'){
+    endpoint = 'wss://kusama-rpc.polkadot.io';
   } else if (network == 'westend'){
-    const wsProvider = new WsProvider('wss://westend-rpc.polkadot.io');
+    endpoint = 'wss://westend-rpc.polkadot.io';
   } else if (network == 'rococo'){
-  const wsProvider = new WsProvider('wss://rococo-rpc.polkadot.io');
+    endpoint = 'wss://rococo-rpc.polkadot.io';
   } else if (network == 'astar'){
-    const wsProvider = new WsProvider('wss://rpc.astar.network');
+    endpoint = 'wss://rpc.astar.network';
   } else if (network == 'shibuya'){
-    const wsProvider = new WsProvider('wss://rpc.shibuya.astar.network');
+    endpoint = 'wss://rpc.shibuya.astar.network';
+  } else {
+    endpoint = 'wss://rpc.polkadot.io';
   }
+  let wsProvider = new WsProvider(endpoint);
 
   const api = await ApiPromise.create({ provider: wsProvider });
 
